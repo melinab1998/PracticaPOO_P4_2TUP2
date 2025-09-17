60a86424129944075af6bc3613ee329fc9c26405
Melina Bueno <melinabueno123@gmail.com>
2025-09-17 14:58:50 -0300

    fix-97

--- src/Web/Controllers/BankAccountController.cs
+++ src/Web/Controllers/BankAccountController.cs
@@ -102,7 +102,7 @@ public class BankAccountController : ControllerBase
     {
         try
         {
-            var account = accounts.FirstOrDefault(a => a.Number == accountNumber);
+            var account = _context.bankAccounts.FirstOrDefault(a => a.Number == accountNumber);
 
             if (account == null)
                 return NotFound("Cuenta no encontrada.");
